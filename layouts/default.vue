3660f0f53817cf40f2ae152ee5b9a12f7ffc1069
s1rojov <sirojoff2727@gmail.com>
2024-04-13 22:11:33 +0500

    git init

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class="py-5 bg-red-500 w-full">
+            this header
+        </div>
+        <slot />
+        <div class="py-5 bg-blue-500 w-full">
+            this footer
+        </div>
+    </div>
+</template>
